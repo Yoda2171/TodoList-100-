a2f49763bb95a0361633ca037a249565e5cb90c7
Yoda2171 <popotaya7@gmail.com>
2021-06-23 04:17:02 -0400

    todo list 1000%

--- src/App.js
+++ src/App.js
@@ -3,28 +3,32 @@ import './App.css';
 
 function App() {
 
-  const [input, setInput] = useState()
+  const [value, setValue] = useState()
   const [task, setTask] = useState([])
-  const data= useRef(null) 
+  const input = useRef(null)
 
   function handlesubmit(e) {
-    setInput(e.target.value)
-
-    if(e.keyCode===13){
-      addTask(input)
+    setValue(e.target.value)
+    if (e.keyCode === 13) {
+      addTask(e.target.value)
+      e.target.value = ""
     }
   }
 
-  function addTask(input) {
-    const add = task.concat(input)
-    setTask(add)
-    data.current.value=""
+  function addTask(value) {
+    if (value !== "") {
+      const add = task.concat(value)
+      setTask(add)
+      input.current.value = ""
+      setValue([])
+    }
+
   }
 
-  
   function deleteTask(index) {
-    task.splice(index,1)
+    task.splice(index, 1)
     addTask([])
+
   }
 
   return (
@@ -32,9 +36,9 @@ function App() {
       <div className="container p-5">
         <div className="p-3 mb-2 bg-primary text-white rounded text-center"><h1>Todo List</h1></div>
         <div className="input-group mb-3">
-          <input ref={data} type="text" className="form-control" placeholder="Insert Task" aria-label="Recipient's username" aria-describedby="button-addon2" onKeyUp={handlesubmit} />
+          <input ref={input} type="text" className="form-control" placeholder="Insert Task" aria-label="Recipient's username" aria-describedby="button-addon2" onKeyUp={handlesubmit} />
           <div className="input-group-append">
-            <button className="btn btn-outline-success" type="button" id="button-addon2" onClick={() => addTask(input)}>Add</button>
+            <button className="btn btn-outline-success" type="button" id="button-addon2" onClick={() => addTask(value)}>Add</button>
           </div>
         </div>
 
@@ -45,7 +49,7 @@ function App() {
               task.map((value, index) => {
                 return (
                   <>
-                    <p key={index} class="card-text">{value}<div class="btn btn-group ml-auto" role="group" aria-label="..." onClick={() => deleteTask(index)}><i class="fas fa-times-circle text-danger "></i></div></p>
+                    <p key={index} class="card-text">{value}<div class="btn btn-group input-group-append" role="group" aria-label="..." onClick={() => deleteTask(index)}><i class="fas fa-times-circle text-danger "></i></div></p>
                   </>
                 )
               })
